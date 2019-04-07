2206760a2e0b025d9860e6abc3d09439227ef3e8
BuildTools <unconfigured@null.spigotmc.org>
2019-04-07 17:52:03 +0200

    1.0.1

---
+++ src/main/java/me/towaha/nshc/ChatManager.java
@@ -0,0 +1,20 @@
+package me.towaha.nshc;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChatManager {
+    public ChatManager
+
+    public static void sendConsoleMessage(String msg) {
+
+    }
+
+    public static void sendMessage(String msg) {
+
+    }
+
+    public static void sendMessage(String msg, Player player) {
+
+    }
+}
