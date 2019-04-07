253d4e3f93f6a6e8aa48fb0ed149942074be995f
Noah Verkaik <noahverkaik@gmail.com>
2019-04-07 19:46:38 +0200

    Update PlayerListManager.java

--- src/main/java/me/towaha/nshc/PlayerListManager.java
+++ src/main/java/me/towaha/nshc/PlayerListManager.java
@@ -18,6 +18,6 @@ public class PlayerListManager {
 
     public void updatePlayerList(Player player) {
         // Update the player list for the player here.
-        player.setPlayerListHeaderFooter("§b§lWelcome to §6§lXenos§!", "§b§lPlayers online: §e" + main.getServer().getOnlinePlayers().size());
+        player.setPlayerListHeaderFooter("§b§lWelcome to §6§lXenos§!", "§b§lPlayers online: §e§l" + main.getServer().getOnlinePlayers().size());
     }
 }
