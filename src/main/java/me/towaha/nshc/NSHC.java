e2cab8074be262f3b613d7beb1d9290de9c7a52e
BuildTools <unconfigured@null.spigotmc.org>
2019-04-07 17:47:21 +0200

    1.0.0

---
+++ src/main/java/me/towaha/nshc/NSHC.java
@@ -0,0 +1,17 @@
+package me.towaha.nshc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NSHC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
