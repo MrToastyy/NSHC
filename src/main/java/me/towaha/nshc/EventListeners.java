1348f1c8e3d1d2dcbf16e40ac025fd02a8f56a5c
Noah Verkaik <noahverkaik@gmail.com>
2019-04-07 18:37:50 +0200

    EventLIstener, PlayerListManager

---
+++ src/main/java/me/towaha/nshc/EventListeners.java
@@ -0,0 +1,30 @@
+package me.towaha.nshc;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class EventListeners implements Listener {
+    NSHC main;
+
+    public EventListeners(NSHC main) {
+        this.main = main;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        e.setJoinMessage("§8[§2+§8] §a" + e.getPlayer());
+
+        main.lm.updatePlayerList();
+        main.sm.updateScoreboard();
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        e.setQuitMessage("§8[§4-§8] §c" + e.getPlayer());
+
+        main.lm.updatePlayerList();
+        main.sm.updateScoreboard();
+    }
+}
--- src/main/java/me/towaha/nshc/NSHC.java
+++ src/main/java/me/towaha/nshc/NSHC.java
@@ -4,14 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NSHC extends JavaPlugin {
 
+    public EventListeners el;
     public ChatManager cm;
     public ScoreboardManager sm;
+    public PlayerListManager lm;
 
     @Override
     public void onEnable() {
         // Create instances of all the available managers.
         cm = new ChatManager(this);
         sm = new ScoreboardManager(this);
+        el = new EventListeners(this);
+        lm = new PlayerListManager(this);
+
+        // Register the events class as an event handler.
+        getServer().getPluginManager().registerEvents(el, this);
 
         cm.sendConsoleMessage("Plugin has finished loading.");
     }
